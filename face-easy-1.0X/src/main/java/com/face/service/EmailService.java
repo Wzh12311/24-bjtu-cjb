d464c2712c7df39f7223d465b6740982067d769f
Wzh12311 <161865770+Wzh12311@users.noreply.github.com>
2024-07-14 22:31:13 +0800

    Delete face-easy-1.0X directory

--- face-easy-1.0X/src/main/java/com/face/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.face.service;
-
-import com.face.bean.result.FaceResult;
-
-public interface EmailService {
-    void sendEmail(String email);
-
-    FaceResult loginemail(String code);
-}
